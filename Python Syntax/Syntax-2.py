9857fbe8561a1f0b3d33c01d569e1155b895e984
Md Sumon Hossain Khan <121485281+sumon2003@users.noreply.github.com>
2024-01-02 22:45:24 +0600

    Python Syntax

--- Python Syntax/Syntax-2.py
+++ Python Syntax/Syntax-2.py
@@ -1,3 +1,6 @@
+"""   بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ   """
+#==->  Md Sumon Hossain Khan <-==#  
+
 if 5 > 2:
     print("5 is greater than 2!")
 if 5 > 2:
@@ -5,7 +8,7 @@ if 5 > 2:
 
 """
 Syntax Error: You have to use the same number of spaces in the same block of code.
-              Otherwise Python will give you an error.
+              Otherwise, Python will give you an error.
 
 if 5 > 2: 
     print("5 is greater than 2!")    
